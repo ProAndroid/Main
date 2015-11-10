5f80f802a08901e76e021f440f0174d06615a08b
facuferreyralpv <facuferreyralp@gmail.com>
2015-11-10 08:52:23 -0300

    actualizacion delas clases

---
+++ app/src/main/java/com/example/tonio/rentallmanager/Cabana.java
@@ -0,0 +1,17 @@
+package com.example.tonio.rentallmanager;
+
+/**
+ * Created by facu on 10/11/15.
+ */
+public class Cabana {
+
+
+    //Nombre de la tabla
+    public final static String TABLE = "cabanas";
+    //Set de los Keys para la base de datos
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "nombre";
+
+    public int cabaña_ID;
+    public String name;
+}
